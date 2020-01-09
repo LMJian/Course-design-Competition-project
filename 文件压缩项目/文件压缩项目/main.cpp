682016fe92ca1392bdcfacc2cac365bc54b3434e
LMJian <1150696681@qq.com>
2020-01-09 18:37:15 +0800

    2020-1-9-更新了项目代码

--- 文件压缩项目/文件压缩项目/main.cpp
+++ 文件压缩项目/文件压缩项目/main.cpp
@@ -1,13 +1,23 @@
-#include"HuffManTree.hpp"
+#define _CRT_SECURE_NO_WARNINGS 1
+#include"FileCompressionHuffManTree.hpp"
+#include"FileExtractionHuffmanTree.hpp"
+
+void TestFileCompression() {       //压缩文件
+	FileCompressionHuffManTree tree;  //指定需要压缩的文件位置
+	tree.InitQue();                //打开文件，统计每个字符的个数，然后创建结点，压入优先级队列
+	tree.InitTree();               //初始化二叉树
+	tree.WriteBin();               //开始压缩
+}
+
+void TestFileExtraction() {        //解压文件
+	FileExtractionHuffManTree tree;
+	tree.InitTree();          //根据头部信息，重建二叉树
+}
 
 int main() {
-	//获取所压缩文件的内容
+	TestFileCompression();
 
-	//建立哈夫曼树，并获取每个字符对应的密码
-	HuffManTree tree("test.txt");
-	tree.InitQue();
-	tree.InitTree();
-	//开始压缩文件并保存
+	TestFileExtraction();
 
 	system("pause");
 	return 0;
